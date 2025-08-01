a2b7488e7bca51fef7a4ba1f34add24af3c27dce
Isaac-bueno-oliveira <olivr145@gmail.com>
2025-08-01 11:02:41 -0300

    Segundo commit.

--- ProjetoEclipseGitHub/src/integracao/Teste.java
+++ ProjetoEclipseGitHub/src/integracao/Teste.java
@@ -4,6 +4,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Primeira integração");
+		System.out.println("Segunda integração");
+
 
 	}
 
